deed502f2b6113b320212e1bdfb1c6a7e6f4a410
mYnO <human350@gmail.com>
2023-07-11 18:02:43 +0900

    click

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
-def main(args):
+def main():
     pygame.init()
     fps=30
     fpsClock = pygame.time.Clock()
@@ -17,10 +17,14 @@ def main(args):
     screen = pygame.display.set_mode((width, height),pygame.FULLSCREEN)
     screen.fill((0,0,0))
 
-    smartfarm=SmartFarmControl.SmartFarmControl()
-    smartfarm.initializing_end_to_end()
+    # smartfarm=SmartFarmControl.SmartFarmControl()
+    # smartfarm.initializing_end_to_end()
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button==1:
+                pygame.quit()
+                sys.exit()
         pygame.display.flip()
         fpsClock.tick(fps)
 
